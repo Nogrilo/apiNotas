c41e3e6f2edb5978405fc46a04b59b8e78e7fdd4
Nogrilo <nogriloesirjeferson@gmail.com>
2025-12-10 21:59:25 -0300

    ajustando as rotas

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const EstudanteDAO = require('../DAO/EstudanteDAO');
 
-router.post('/estudante/cadastrar', EstudanteDAO.cadastrar);
+router.post('/notas/inserir', EstudanteDAO.cadastrar);
 router.delete('/estudante/excluir/:nome', EstudanteDAO.excluir);
 router.get('/estudante', EstudanteDAO.listar);
 router.post('/estudante/alterar/:nome', EstudanteDAO.alterar);
